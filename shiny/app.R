6fc3e0ae3b48415b8b93cdd4700b898db773c5c3
jlmatt <josephmatt13@gmail.com>
2024-09-20 15:01:49 -0500

    updated draft of shiny app, before trying plotly

--- shiny/app.R
+++ shiny/app.R
@@ -3,34 +3,47 @@ library(bslib)
 library(ggplot2)
 library(tidyverse)
 library(here)
+library(leaflet)
+library(plotly)
 
 # Load data
 data <- readRDS(here("shiny", "data", "data_for_shiny.rds"))
 fec <- readRDS(here("shiny", "data", "weight_adjusted_fecundity.rds"))
-fem <- readRDS(here("shiny", "data", "number_of_females.rds"))
+rat <- readRDS(here("shiny", "data", "rat.rds"))
+rep <- readRDS(here("shiny", "data", "rep.rds"))
+temp <-readRDS(here("shiny", "data", "temp.rds"))
+sal <-readRDS(here("shiny", "data", "sal.rds"))
 legend <- readRDS(here("shiny", "data", "data_for_scale_manual.rds"))
+
 
 
 # Define UI
 ui <- fluidPage(
-  titlePanel("PMAR"),
+  titlePanel("PMAR Wild Oyster Survey"),
   sidebarLayout(
     sidebarPanel(
       # Dropdown to select data type
       selectInput("data_type", "Select Data Type:",
-                  choices = c("Fecundity", "Females", "Males"), # Replace with your actual data types
-                  selected = "Fecundity"),  # Default selection
+                  choices = c("Female Fecundity", 
+                              "Sperm Rating", 
+                              "Gametogenically Active",
+                              "Temperature",
+                              "Salinity"), # Replace with your actual data types
+                  selected = "Female Fecundity"),  # Default selection
       
       # Checkbox group for selecting bays
       checkboxGroupInput("selected_bays", "Select Bays:", 
                          choices = unique(data$bay_full_name), 
-                         selected = unique(data$bay_full_name)),
+                         selected = data %>% 
+                           filter(bay %in% c("GAL","LLM")) %>%
+                           pull(bay_full_name)),
       # Dynamic selection for sites based on selected bays
       uiOutput("site_selection")  # Placeholder for dynamic site selection
     ),
     
     mainPanel(
-      plotOutput("linePlot")  # Space for the plot
+      plotOutput("linePlot"),  # Space for the plot
+      leafletOutput("map")      # Space for the map
     )
   )
 )
@@ -42,10 +55,16 @@ server <- function(input, output, session) {
   filteredData <- reactive({
     req(input$selected_bays)
     
-    if (input$data_type == "Fecundity") {
+    if (input$data_type == "Female Fecundity") {
       fec %>% filter(bay_full_name %in% input$selected_bays)
-    } else if (input$data_type == "Females") {
-      fem %>% filter(bay_full_name %in% input$selected_bays)
+    } else if (input$data_type == "Sperm Rating") {
+      rat %>% filter(bay_full_name %in% input$selected_bays)
+    } else if (input$data_type == "Gametogenically Active") {
+      rep %>% filter(bay_full_name %in% input$selected_bays)
+    } else if (input$data_type == "Temperature") {
+      temp %>% filter(bay_full_name %in% input$selected_bays)
+    } else if (input$data_type == "Salinity") {
+      sal %>% filter(bay_full_name %in% input$selected_bays)
     }
   })
   
@@ -79,7 +98,7 @@ server <- function(input, output, session) {
     # Convert key to a factor with levels in the desired order
     data_to_plot$key <- factor(data_to_plot$key, levels = filtered_legend$key)
     
-    if (input$data_type == "Fecundity") {
+    if (input$data_type == "Female Fecundity") {
       ggplot(data_to_plot, aes(x = date , y = md, 
                       fill= key,
                       shape = key,
@@ -102,26 +121,131 @@ server <- function(input, output, session) {
           date_labels = "%b"                        
         ) + 
         ylab("Eggs Per Grams of Tissue") +
+        xlab("Date") +
         theme_minimal()
       
-    } else if (input$data_type == "Females") {
-      ggplot(data_to_plot, aes(x = date , y = n, 
-                      fill= fct_reorder(key,latitude, .desc = TRUE),
-                      shape = fct_reorder(key,latitude, .desc = TRUE),
-                      color = fct_reorder(bay,latitude, .desc = TRUE))) +
+    } else if (input$data_type == "Sperm Rating") {
+      ggplot(data_to_plot, aes(x = date , y = rating, 
+                               fill= key,
+                               shape = key,
+                               color = key)) +
         geom_point(size = 5, stroke = 1, color = "black") + 
         geom_line(linewidth = 1) +
-        scale_color_manual(values = legend$color,guide = "none") +
+        scale_color_manual(values = filtered_legend$color, guide = "none") +
         scale_fill_manual(name = "Sites",
-                          labels = legend$key,
-                          values = legend$color) + 
+                          labels = filtered_legend$key,
+                          values = filtered_legend$color) + 
         scale_shape_manual(name = "Sites",
-                           labels = legend$key,
-                           values = legend$shape) +
+                           labels = filtered_legend$key,
+                           values = filtered_legend$shape) +
+        scale_y_continuous(limits = c(0,5),
+                           breaks = seq(0, 5, by = 1))+
+        scale_x_date(
+          limits = as.Date(c("2023-11-01", "2024-07-31")),
+          date_breaks = "1 month",              
+          date_labels = "%b"                        
+        ) + 
+        ylab("Mean Sperm Rating") +
+        xlab("Date") +
+        theme_minimal()
+      
+    } else if (input$data_type == "Gametogenically Active") {
+      ggplot(data_to_plot, aes(x = date , y = active, 
+                               fill= key,
+                               shape = key,
+                               color = key)) +
+        geom_point(size = 5, stroke = 1, color = "black") + 
+        geom_line(linewidth = 1) +
+        scale_color_manual(values = filtered_legend$color, guide = "none") +
+        scale_fill_manual(name = "Sites",
+                          labels = filtered_legend$key,
+                          values = filtered_legend$color) + 
+        scale_shape_manual(name = "Sites",
+                           labels = filtered_legend$key,
+                           values = filtered_legend$shape) +
+        scale_y_continuous(limits = c(0,1),
+                           breaks = seq(0, 1, by = 0.2))+
+        scale_x_date(
+          limits = as.Date(c("2023-11-01", "2024-07-31")),
+          date_breaks = "1 month",              
+          date_labels = "%b"                        
+        ) + 
+        ylab("Proportion Gametogenically Active") +
+        xlab("Date") +
+        theme_minimal()
+      
+    } else if (input$data_type == "Temperature") {
+      ggplot(data_to_plot, aes(x = date , y = m_temp, 
+                               fill= key,
+                               shape = key,
+                               color = key)) +
+        geom_point(size = 5, stroke = 1, color = "black") + 
+        geom_line(linewidth = 1) +
+        scale_color_manual(values = filtered_legend$color, guide = "none") +
+        scale_fill_manual(name = "Sites",
+                          labels = filtered_legend$key,
+                          values = filtered_legend$color) + 
+        scale_shape_manual(name = "Sites",
+                           labels = filtered_legend$key,
+                           values = filtered_legend$shape) +
+        scale_y_continuous(limits = c(0,35),
+                           breaks = seq(0, 35, by = 10))+
+        scale_x_date(
+          limits = as.Date(c("2023-11-01", "2024-07-31")),
+          date_breaks = "1 month",              
+          date_labels = "%b"                        
+        ) + 
+        ylab("Temperature in Celcius") +
+        xlab("Date") +
+        theme_minimal()
+      
+    } else if (input$data_type == "Salinity") {
+      ggplot(data_to_plot, aes(x = date , y = m_sal, 
+                               fill= key,
+                               shape = key,
+                               color = key)) +
+        geom_point(size = 5, stroke = 1, color = "black") + 
+        geom_line(linewidth = 1) +
+        scale_color_manual(values = filtered_legend$color, guide = "none") +
+        scale_fill_manual(name = "Sites",
+                          labels = filtered_legend$key,
+                          values = filtered_legend$color) + 
+        scale_shape_manual(name = "Sites",
+                           labels = filtered_legend$key,
+                           values = filtered_legend$shape) +
+        scale_y_continuous(limits = c(0,55),
+                           breaks = seq(0, 55, by = 10))+
+        scale_x_date(
+          limits = as.Date(c("2023-11-01", "2024-07-31")),
+          date_breaks = "1 month",              
+          date_labels = "%b"                        
+        ) + 
+        ylab("Salinity (ppt)") +
+        xlab("Date") +
         theme_minimal()
     }
     
   })
+  
+  output$map <- renderLeaflet({
+    req(input$selected_sites)  # Ensure sites are selected
+    site_data <- data %>% filter(site_full_name %in% input$selected_sites)
+    
+    leaflet(site_data) %>%
+      addProviderTiles(providers$CartoDB.PositronNoLabels) %>%
+      addCircleMarkers(lng = ~longitude, 
+                       lat = ~latitude, 
+                       popup = ~site_full_name,
+                       fillColor = ~color,
+                       color = "black",
+                       weight = 0.5,
+                       radius = 5) %>%
+      fitBounds(lng1 = min(data$longitude),
+                lat1 = min(data$latitude),
+                lng2 = max(data$longitude),
+                lat2 = max(data$latitude))
+  }) 
+  
 }
 # Run the application 
 shinyApp(ui = ui, server = server)
